813e7ab1f27f36edc8eb8c48e75ec89490c55037
GentleHorse <anch.abc.t@gmail.com>
2024-03-26 19:07:16 +0100

    Implement react three rapier

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,14 +1,26 @@
 import { OrbitControls } from "@react-three/drei";
+import { Physics, RigidBody } from "@react-three/rapier";
 
 export default function Experience() {
   return (
     <>
       <OrbitControls makeDefault />
 
-      <mesh scale={1.5}>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Physics debug={true}>
+        <RigidBody restitution={0.5}>
+          <mesh scale={0.5} position={[0, 3, 0]}>
+            <boxGeometry />
+            <meshNormalMaterial />
+          </mesh>
+        </RigidBody>
+
+        <RigidBody type="fixed" restitution={0.5}>
+          <mesh scale={[10, 0.1, 10]} position={[0, -1, 0]}>
+            <boxGeometry />
+            <meshBasicMaterial color="snow" />
+          </mesh>
+        </RigidBody>
+      </Physics>
     </>
   );
 }
