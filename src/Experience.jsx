07e30fa073c98e24fd72ce4ee692bc96975d9c8a
GentleHorse <anch.abc.t@gmail.com>
2024-04-07 19:19:38 +0200

    Added emission geometries

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -5,6 +5,7 @@ import { Perf } from "r3f-perf";
 import TestFloor from "./components/test/TestFloor.jsx";
 import CharacterControl from "./components/character/CharacterControl.jsx";
 import PostProcessingEffects from "./components/postprocessing/PostProcessingEffects.jsx";
+import TestGeometriesEmission from "./components/test/TestGeometriesEmission.jsx";
 
 export default function Experience() {
   const [isSceneReady, setIsSceneReady] = useState(false);
@@ -34,21 +35,29 @@ export default function Experience() {
       <Physics debug={Physics} timeStep="vary">
         <TestFloor />
 
-        <RigidBody position={[-2, 3, 0]}>
-          <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-          </mesh>
-        </RigidBody>
+        <Suspense>
+          <RigidBody position={[-2, 3, 0]}>
+            <mesh>
+              <boxGeometry />
+              <meshNormalMaterial />
+            </mesh>
+          </RigidBody>
+        </Suspense>
 
-        <RigidBody colliders="hull" position={[2, 2, 0]}>
-          <mesh>
-            <icosahedronGeometry />
-            <meshNormalMaterial />
-          </mesh>
-        </RigidBody>
+        <Suspense>
+          <RigidBody colliders="hull" position={[2, 2, 0]}>
+            <mesh>
+              <icosahedronGeometry />
+              <meshNormalMaterial />
+            </mesh>
+          </RigidBody>
+        </Suspense>
 
-        <CharacterControl />
+        <Suspense>
+          <CharacterControl />
+        </Suspense>
+
+        <TestGeometriesEmission />
       </Physics>
     </>
   );
---
+++ src/components/test/TestGeometriesEmission.jsx
@@ -0,0 +1,42 @@
+import { useControls } from "leva";
+import { RigidBody } from "@react-three/rapier";
+import { Suspense } from "react";
+
+export default function TestGeometriesEmission() {
+  const { sphereEmission, cubeEmission } = useControls("emission", {
+    sphereEmission: { value: 1, min: 0, max: 20, step: 0.01 },
+    cubeEmission: { value: 1, min: 0, max: 20, step: 0.01 },
+  });
+
+  return (
+    <>
+      <Suspense>
+        <RigidBody colliders="ball" position={[-1.5, 1, -1.5]} scale={0.9}>
+          <mesh castShadow>
+            <sphereGeometry />
+            <meshBasicMaterial
+              color={[
+                1.5 * sphereEmission,
+                4 * sphereEmission,
+                4 * sphereEmission,
+              ]}
+              toneMapping={false}
+            />
+          </mesh>
+        </RigidBody>
+      </Suspense>
+
+      <Suspense>
+        <RigidBody position={[2, 1, -1]} scale={0.9}>
+          <mesh castShadow>
+            <boxGeometry />
+            <meshBasicMaterial
+              color={[4 * cubeEmission, 1 * cubeEmission, 1.5 * cubeEmission]}
+              toneMapping={false}
+            />
+          </mesh>
+        </RigidBody>
+      </Suspense>
+    </>
+  );
+}
