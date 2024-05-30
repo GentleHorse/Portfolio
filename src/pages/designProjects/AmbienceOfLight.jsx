d77ca1689d41fa8e7702cf209c943cebd334c0f8
GentleHorse <anch.abc.t@gmail.com>
2024-05-30 20:32:26 +0200

    Added project pictures and texts to the Ambience of Light project page

--- src/pages/designProjects/AmbienceOfLight.jsx
+++ src/pages/designProjects/AmbienceOfLight.jsx
@@ -2,16 +2,12 @@ import * as THREE from "three";
 import { useRef, useState } from "react";
 import { Canvas, createPortal, useFrame, useThree } from "@react-three/fiber";
 import {
-  useFBO,
-  useGLTF,
   useScroll,
   Text,
   Image,
   Scroll,
   Preload,
   ScrollControls,
-  MeshTransmissionMaterial,
-  Html,
 } from "@react-three/drei";
 import { easing } from "maath";
 
@@ -21,64 +17,56 @@ import AmbienceOfLightImage06 from "../../../public/images/design-projects/amibi
 import AmbienceOfLightImage07 from "../../../public/images/design-projects/amibience-of-light/ambience-of-light-image-07.jpg";
 import AmbienceOfLightImage11 from "../../../public/images/design-projects/amibience-of-light/ambience-of-light-image-11.jpg";
 import AmbienceOfLightImage13 from "../../../public/images/design-projects/amibience-of-light/ambience-of-light-image-13.jpg";
+import { toneMapping } from "three/examples/jsm/nodes/Nodes.js";
 
 export default function AmbienceOfLightPage() {
   return (
     <Canvas camera={{ position: [0, 0, 20], fov: 15 }}>
-      <ScrollControls damping={0.2} pages={4} distance={0.5}>
+      <ScrollControls damping={0.2} pages={5} distance={0.5}>
+        {/* Canvas contents in here will *not* scroll, but receive useScroll! */}
+
+        {/* <TestCube scale={1.5} position={[0, 0, -2]} /> */}
+
         <Scroll>
-          <group position={[0, 0, 0]}>
-            <mesh>
-              <boxGeometry />
-              <meshBasicMaterial color="red" />
-            </mesh>
-            <Typography />
-          </group>
+          {/* Canvas contents in here will scroll along */}
 
-          <group position={[0, -5, 0]}>
-            <mesh>
-              <boxGeometry />
-              <meshBasicMaterial color="yellow" />
-            </mesh>
-          </group>
-
-          <group position={[0, -10, 0]}>
-            <mesh>
-              <boxGeometry />
-              <meshBasicMaterial color="green" />
-            </mesh>
-          </group>
+          <Typography />
+          <ProjectImages />
         </Scroll>
 
         <Scroll html>
-            <article className="py-[5%] px-[12%]" style={{ transform: 'translate3d(0vw, 300vh, 0)' }}>
-              <p className="font-roboto text-[#fcfaf2]">
-                In his prior artistic endeavor, "Beauty of Time Passing (2023),"
-                the designer undertook the ambitious task of introducing
-                luminosity into interior spaces through the integration of
-                data-driven meteorological landscapes and the strategic
-                manipulation of light dispersion. The intentional use of glass
-                bricks, with their capacity for reflective interplay, served as
-                a conduit for cultivating a symbiotic relationship between the
-                designed environment and the natural world, resulting in a
-                nuanced nature-inspired ambiance within the domestic realm.In
-                his current project, aptly titled "Ambience of Light," the
-                artist remains dedicated to the same overarching goal while
-                adopting a distinct modus operandi. Here, a profound exploration
-                of the materiality intrinsic to wood and glass bricks takes
-                precedence, as the designer endeavors to distill his vision into
-                a more refined yet robust formal language. This deliberate
-                departure from the previous methodology reflects a commitment to
-                simplicity, underscoring a conscientious effort to attain a
-                heightened aesthetic resonance through a judicious consideration
-                of materials and their inherent expressive potential.
-              </p>
-              <br />
-              <p className="font-roboto text-[#fcfaf2]">
-                This project was presented at OBJECT Rotterdam 2024 (Feb 2 - 4,
-                2024, HAKA-gebouw, Vierhavensstraat 40, 3029 BE Rotterdam).
-              </p>
-            </article>
+          {/* DOM contents in here will scroll along */}
+
+          <article
+            className="py-[5%] px-[12%]"
+            style={{ transform: "translate3d(0vw, 420vh, 0)" }}
+          >
+            <p className="font-roboto text-[#fcfaf2]">
+              In his earlier work, "Beauty of Time Passing" (2023), the designer
+              ambitiously sought to illuminate indoor spaces by integrating
+              data-driven weather landscapes and innovative light manipulation.
+              By using glass bricks, known for their reflective qualities, he
+              created a harmonious connection between the interior environment
+              and the natural world, resulting in a sophisticated,
+              nature-inspired ambiance. Now, with his latest project, "Ambience
+              of Light," he continues with the same overarching goal but shifts
+              his approach to make the project more accessible. This time, he
+              focuses on the raw, pure qualities of materials like wood and
+              glass, along with intangible elements like light reflections and
+              dispersions. By carefully selecting and arranging these
+              components, he crafts a much stronger design language. The
+              simplified structure of this project also highlights the direct
+              influences of the designer's Japanese cultural heritage and his
+              personal interpretation of it. This thoughtful approach not only
+              enhances the aesthetic appeal but also makes the project more
+              relatable and meaningful to a broader audience.
+            </p>
+            <br />
+            <p className="font-roboto text-[#fcfaf2]">
+              This project was presented at OBJECT Rotterdam 2024 (Feb 2 - 4,
+              2024, HAKA-gebouw, Vierhavensstraat 40, 3029 BE Rotterdam).
+            </p>
+          </article>
         </Scroll>
       </ScrollControls>
     </Canvas>
@@ -93,26 +81,87 @@ function Typography() {
   );
   const shared = {
     font: "/fonts/shippori-mincho-b1-v21-japanese-800.woff",
-    fontSize: 0.4,
+    fontSize: 0.6,
     letterSpacing: -0.1,
-    color: "black",
+    color: "white",
   };
+
   return (
     <>
       <Text
-        children="Ambience"
-        // anchorX="left"
-        position={[0, -height / 10, 12]}
+        anchorX="left"
+        position={[-width / 2, -height * 0.1, 12]}
         {...shared}
-      />
+      >
+        AMBIENCE
+        <meshBasicMaterial color="white" toneMapped={false} />
+      </Text>
+      <Text anchorX="center" position={[0, -height * 0.8, 12]} {...shared}>
+        OF
+        <meshBasicMaterial color="white" toneMapped={false} />
+      </Text>
       <Text
-        children="of"
-        // anchorX="right"
-        position={[0, -height * 2, 12]}
+        anchorX="right"
+        position={[width / 2.5, -height * 1.5, 12]}
         {...shared}
-      />
-      <Text children="Light" position={[0, -height * 4.624, 12]} {...shared} />
+      >
+        LIGHT
+        <meshBasicMaterial color="white" toneMapped={false} />
+      </Text>
     </>
+  );
+}
+
+function ProjectImages() {
+  const state = useThree();
+  const { width, height } = state.viewport.getCurrentViewport(
+    state.camera,
+    [0, 0, 12]
+  );
+
+  return (
+    <>
+      <Image
+        url={AmbienceOfLightImage13}
+        scale={[14.4 * 0.5, 9.6 * 0.5, 1]}
+        position={[-width / 1.5, 0, -5]}
+      />
+
+      <Image
+        url={AmbienceOfLightImage07}
+        scale={[14.4 * 0.5, 9.6 * 0.5, 1]}
+        position={[width / 2.5, -height * 2.5, -1]}
+      />
+
+      <Image
+        url={AmbienceOfLightImage11}
+        scale={[14.4 * 0.5, 9.6 * 0.5, 1]}
+        position={[-width / 1.5, -height * 5, -10]}
+      />
+
+      <Image
+        url={AmbienceOfLightImage01}
+        scale={[14.4 * 0.5, 9.6 * 0.5, 1]}
+        position={[0, -height * 8, 1]}
+      />
+    </>
+  );
+}
+
+function TestCube(props) {
+  const testCube = useRef();
+
+  const scroll = useScroll();
+
+  useFrame((state, delta) => {
+    testCube.current.rotation.y = Math.PI * 2 * scroll.offset;
+  });
+
+  return (
+    <mesh {...props} ref={testCube}>
+      <boxGeometry />
+      <meshNormalMaterial />
+    </mesh>
   );
 }
 
