aebedc29ec3a3487aef1312000fd8743391ea9f6
GentleHorse <anch.abc.t@gmail.com>
2024-07-12 23:11:09 +0200

    Added the Masu Typo, Comforting Dinner project models

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -20,6 +20,8 @@ import FirstPersonViewControl from "./components/characterControl/firstPersonVie
 import AmbienceOfLight from "./components/models/designWorks/ambienceOfLight/AmbienceOfLight.jsx";
 import BeautyOfTimePassing from "./components/models/designWorks/beautyOfTimePassing/BeautyOfTimePassing.jsx";
 import InterventionInOurDisconnection from "./components/models/designWorks/interventionInOurDisconnection/InterventionInOurDisconnection.jsx";
+import MasuTypo from "./components/models/designWorks/masuTypo/MasuTypo.jsx";
+import ComfortingDinner from "../ComfortingDinner.jsx";
 
 
 export default function Experience() {
@@ -64,6 +66,8 @@ export default function Experience() {
         <AmbienceOfLight scale={0.5} position={[-2, 0, 2]} />
         <BeautyOfTimePassing scale={0.25} rotation={[0, -Math.PI * 0.1, 0]} position={[3, 0, -15]} />
         <InterventionInOurDisconnection scale={0.5} rotation={[0, Math.PI * 0.1, 0]} position={[-4, 0, -40]} />
+        <MasuTypo scale={0.3} position={[4, 0, -65]} />
+        <ComfortingDinner scale={0.5} rotation={[0, Math.PI * 0.2, 0]} position={[-5, 0, -100]}  />
 
         {/* <AmbienceOfLight scale={0.3} rotation={[0, Math.PI, 0]} /> */}
       </Physics>
