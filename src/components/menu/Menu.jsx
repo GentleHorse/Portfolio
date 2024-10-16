add89b66661a4722255762e19c1f04905a2a4ac0
GentleHorse <anch.abc.t@gmail.com>
2024-10-16 23:50:46 +0200

    [home] page - browser - added the intro texts and portal explanation to the Menu component

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -71,6 +71,168 @@ export default function Menu() {
     };
   }, []);
 
+  /**
+   * CONTENT PAGE STATE, HANDLER ARRAY
+   */
+  const [contentPage, setContentPage] = useState(0);
+  const contentPageIncrementHandler = () => {
+    setContentPage((prevContentPageNum) => {
+      if (prevContentPageNum === 2) {
+        prevContentPageNum = 0;
+      } else {
+        prevContentPageNum += 1;
+      }
+
+      return prevContentPageNum;
+    });
+  };
+  const contentPageDecrementHandler = () => {
+    setContentPage((prevContentPageNum) => {
+      prevContentPageNum -= 1;
+      return prevContentPageNum;
+    });
+  };
+  const contentsArray = [
+    {
+      id: "c1",
+      content: (
+        <section className="flex flex-col items-center">
+          <div className="w-full mt-5 px-[120px] text-slate-50 text-md">
+            <p className="mb-10 text-center text-slate-50 font-serif text-3xl">
+              Welcome!
+            </p>
+
+            <div>
+              <p className="my-5 text-slate-50 font-montserrat text-xl">
+                This is the gallery for my physical design works and the working
+                studio for my 3D digital works.
+              </p>
+              <p className="my-5 text-slate-50 font-montserrat text-xl">
+                You can freely walk around with your mouse and keyboard keys.
+              </p>
+            </div>
+          </div>
+
+          <button
+            className="px-1 py-4 my-10 flex justify-evenly items-center w-[170px] h-1/7 rounded-full bg-[#09090985] hover:bg-[#565d7595] text-slate-50 uppercase font-serif font-extrabold text-xl"
+            onClick={contentPageIncrementHandler}
+          >
+            <p>n e x t</p>
+            <img src="./images/icons/play-triangle.svg" className="w-[35px]" />
+          </button>
+        </section>
+      ),
+    },
+    {
+      id: "c2",
+      content: (
+        <section className="flex flex-col items-center">
+          <div className="w-full mt-5 px-[120px] text-slate-50 text-md">
+            <p className="mb-10 text-center text-slate-50 font-serif text-3xl">
+              How to Play
+            </p>
+
+            <div className="flex justify-between mb-10 py-12 border rounded-xl">
+              <div className="mx-4 flex flex-col items-center justify-center">
+                <img src={moveKeysIcon} className="w-20 h-16" />
+                <p className="m-1 font-montserrat text-md text-center">
+                  WASD/AQSD/arrow keys to move
+                </p>
+              </div>
+              <div className="mx-4 flex flex-col items-center justify-center">
+                <img src={mouseIcon} className="w-16 h-16" />
+                <p className="m-1 font-montserrat text-md text-center">
+                  Mouse to change the camera angle
+                </p>
+              </div>
+              <div className="mx-4 flex flex-col items-center justify-center">
+                <img src={escKeyIcon} className="w-16 h-16" />
+                <p className="m-1 font-montserrat text-md text-center">
+                  Esc key to go back to Menu
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-10">
+            <button
+              className="px-1 py-4 my-10 flex justify-evenly items-center w-[170px] h-1/7 rounded-full bg-[#09090985] hover:bg-[#565d7595] text-slate-50 uppercase font-serif font-extrabold text-xl"
+              onClick={contentPageDecrementHandler}
+            >
+              <img
+                src="./images/icons/play-triangle-reverse.svg"
+                className="w-[35px]"
+              />
+              <p>b a c k</p>
+            </button>
+            <button
+              className="px-1 py-4 my-10 flex justify-evenly items-center w-[170px] h-1/7 rounded-full bg-[#09090985] hover:bg-[#565d7595] text-slate-50 uppercase font-serif font-extrabold text-xl"
+              onClick={contentPageIncrementHandler}
+            >
+              <p>n e x t</p>
+              <img
+                src="./images/icons/play-triangle.svg"
+                className="w-[35px]"
+              />
+            </button>
+          </div>
+        </section>
+      ),
+    },
+    {
+      id: "c3",
+      content: (
+        <section className="flex flex-col items-center">
+          <div className="w-full mt-5 px-[120px] text-slate-50 text-md">
+            <p className="mb-10 text-center text-slate-50 font-serif text-3xl">
+              Portal for explore more
+            </p>
+
+            <div className="flex justify-between gap-8 mb-10 py-10">
+              <div>
+                <p className="mb-5 font-montserrat text-md">
+                  There are "portals" in this space.
+                </p>
+                <p className="mb-24 font-montserrat text-md">
+                  If you feel like knowing more about the project, you can visit
+                  individual project pages by entering this "portal" gate.
+                </p>
+                <p className="font-great-vibes text-4xl">
+                  Enjoy!
+                </p>
+              </div>
+
+              <img src="./images/menu/portal-gate.jpg" className="h-[300px]" />
+            </div>
+          </div>
+
+          <div className="flex gap-10">
+            <button
+              className="px-1 py-4 my-10 flex justify-evenly items-center w-[170px] h-1/7 rounded-full bg-[#09090985] hover:bg-[#565d7595] text-slate-50 uppercase font-serif font-extrabold text-xl"
+              onClick={contentPageDecrementHandler}
+            >
+              <img
+                src="./images/icons/play-triangle-reverse.svg"
+                className="w-[35px]"
+              />
+              <p>b a c k</p>
+            </button>
+            <button
+              className="px-1 py-4 my-10 flex justify-evenly items-center w-[170px] h-1/7 rounded-full bg-[#09090985] hover:bg-[#565d7595] text-slate-50 uppercase font-serif font-extrabold text-xl"
+              onClick={modalCloseHandler}
+            >
+              <p>P l a y</p>
+              <img
+                src="./images/icons/play-triangle.svg"
+                className="w-[35px]"
+              />
+            </button>
+          </div>
+        </section>
+      ),
+    },
+  ];
+
   return (
     <>
       <Modal
@@ -79,34 +241,10 @@ export default function Menu() {
         className="w-[800px] h-[80vh] overflow-hidden rounded-2xl backdrop-blur-md bg-[#C1C1C1]/15"
       >
         <section className="w-full h-full flex flex-col items-center justify-evenly">
-          <div className="w-full mt-5 px-[150px] text-slate-50 text-md">
-            <p className="mb-10 text-center text-slate-50 font-great-vibes text-3xl">
-              How to Play
-            </p>
+          {/* MAIN SECTION */}
+          {contentsArray[contentPage].content}
 
-            <div className="flex justify-between mb-10 py-10 border rounded-xl">
-              <div className="mx-4 flex flex-col items-center justify-center">
-                <img src={moveKeysIcon} className="w-20 h-14" />
-                <p className="m-1 text-sm text-center">WASD/AQSD/arrow keys to move</p>
-              </div>
-              <div className="mx-4 flex flex-col items-center justify-center">
-                <img src={mouseIcon} className="w-14 h-14" />
-                <p className="m-1 text-sm text-center">Mouse to change the camera angle</p>
-              </div>
-              <div className="mx-4 flex flex-col items-center justify-center">
-                <img src={escKeyIcon} className="w-14 h-14" />
-                <p className="m-1 text-sm text-center">Esc key to go back to Menu</p>
-              </div>
-            </div>
-          </div>
-
-          <button
-            className="px-5 py-5 mb-10 flex justify-evenly items-center w-[200px] h-1/7 rounded-full bg-[#09090985] text-slate-50 uppercase font-serif font-extrabold text-2xl"
-            onClick={modalCloseHandler}
-          >
-            <p>P l a y</p>
-            <img src="./images/icons/play-triangle.svg" className="w-[50px]" />
-          </button>
+          {/* LINKS FOOTER */}
           <div className="w-full text-slate-50 text-xl flex justify-center">
             <Link to="/portfolio-website" reloadDocument={true}>
               <p className="mx-5 font-serif">About this website</p>
