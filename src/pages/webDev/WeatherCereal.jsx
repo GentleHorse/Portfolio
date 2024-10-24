5e6547753cb8ebafed1462ef175db857cb30c639
GentleHorse <anch.abc.t@gmail.com>
2024-10-24 18:59:22 +0200

    [weather cereal] page - finish development

--- src/pages/webDev/WeatherCereal.jsx
+++ src/pages/webDev/WeatherCereal.jsx
@@ -9,7 +9,55 @@ import "lenis/dist/lenis.css";
 import WeatherCerealHeroImage from "../../../public/images/app-developments/__thumbnail-images/thumbnail-weather-cereal.jpg";
 import DonutsUniverseHeroImage from "../../../public/images/app-developments/__thumbnail-images/thumbnail-donuts-universe.jpg";
 
+import WeatherCerealImage01 from "../../../public/images/app-developments/weather-cereal/weather-cereal-image-01.jpg";
+import WeatherCerealImage02 from "../../../public/images/app-developments/weather-cereal/weather-cereal-image-02.jpg";
+import WeatherCerealImage03 from "../../../public/images/app-developments/weather-cereal/weather-cereal-image-03.jpg";
+import WeatherCerealImage04 from "../../../public/images/app-developments/weather-cereal/weather-cereal-image-04.jpg";
+
+import WeatherCerealVideo01 from "../../../public/videos/weather-cereal/weather-cereal-01.mp4";
+
+const IMAGES_ARRAY = [
+  WeatherCerealImage01,
+  WeatherCerealImage02,
+  WeatherCerealImage03,
+  WeatherCerealImage04,
+];
+
+const VIDEOS_ARRAY = [WeatherCerealVideo01];
+
 export default function WeatherCerealPage() {
+  /**
+   * IMAGES ARRAY
+   */
+  const [imagesArray, setImagesArray] = useState([]);
+
+  useEffect(() => {
+    setImagesArray((prevImagesArray) => {
+      const newImagesArray = [...prevImagesArray];
+      IMAGES_ARRAY.forEach((image) => {
+        newImagesArray.push(image);
+      });
+
+      return newImagesArray;
+    });
+  }, []);
+
+  /**
+   * VIDEOS ARRAY
+   */
+  const [videosArray, setVideosArray] = useState([]);
+
+  useEffect(() => {
+    setVideosArray((prevVideosArray) => {
+      const newVideosArray = [...prevVideosArray];
+      VIDEOS_ARRAY.forEach((video) => {
+        newVideosArray.push(video);
+      });
+
+      return newVideosArray;
+    });
+  }, []);
+
   /**
    * MOMENTUM SMOOTH SCROLLING - LENIS SETUP
    */
@@ -19,7 +67,7 @@ export default function WeatherCerealPage() {
     setLoadedStatus(document.readyState === "complete");
     console.log("LoadedStauts: ", loadedStatus);
 
-    // if (videosArray.length === 9) {
+    if (imagesArray.length === 4 & videosArray.length === 1) {
       // Initialize Lenis
       const lenis = new Lenis();
 
@@ -32,7 +80,7 @@ export default function WeatherCerealPage() {
       requestAnimationFrame(raf);
 
       console.log(`Set Lenis`);
-    // }
+    }
   });
 
   /**
@@ -71,14 +119,107 @@ export default function WeatherCerealPage() {
             <h1 className="project-title mb-[21px] xl:mb-[42px] font-serif text-[36px] xl:text-[72px] text-[#C1C1C1]">
               Weather Cereal
             </h1>
+            <img
+              src={WeatherCerealHeroImage}
+              alt="Ambience of Light Hero Image"
+              className="mb-[25px] xl:mb-[70px] w-full h-[200px] xl:h-[500px] object-cover"
+            />
+            <div className="flex flex-col xl:flex-row">
+              <div className="flex-none w-auto xl:w-[350px]">
+                <p className="mb-[4px] xl:mb-[26px] text-[14px] xl:text-[24px] text-[#C1C1C1]">
+                  2024
+                </p>
+                <div>
+                  <p className="mb-[20px] xl:mb-[26px] text-[14px] xl:text-[24px] text-[#C1C1C1]">
+                    3D Design
+                    <br /> + Web Development
+                  </p>
+                  <button
+                    className="mt-0 xl:mt-10 mb-10 xl:mb-16 border-2 rounded-xl hover:bg-[#C1C1C1]"
+                    onClick={() =>
+                      window.open("https://weather-3d-forecast.vercel.app/")
+                    }
+                  >
+                    <p className="text-center mx-4 my-2 xl:px-2 py-1 xl:py-2 font-light font-roboto text-[10px] xl:text-[15px] text-[#C1C1C1] hover:text-[#ffffff]">
+                      Launch Project
+                    </p>
+                  </button>
+                </div>
+              </div>
+
+              <p className="mb-[36px] xl:mb-[72px] xl:grow text-[18px] xl:text-[28px] text-[#C1C1C1]">
+                "Weather Cereal" is a 3D web application that presents a 7-day
+                weather forecast in the style of a morning newspaper, complete
+                with an animated cereal bowl beside it, recreating a familiar
+                breakfast scene with a bit of joke.
+              </p>
+            </div>
             <div className="flex flex-col xl:flex-row gap-2 xl:gap-4">
-              <p className="text-[14px] text-[#C1C1C1]">#software01</p>
-              <p className="text-[14px] text-[#C1C1C1]">#software02</p>
-              <p className="text-[14px] text-[#C1C1C1]">#software03</p>
+              <p className="text-[14px] text-[#C1C1C1]">#weather forecast</p>
+              <p className="text-[14px] text-[#C1C1C1]">
+                #nostalgia of old style
+              </p>
+              <p className="text-[14px] text-[#C1C1C1]">#a joke to spark</p>
             </div>
           </section>
 
+          <section className="mt-[120px] mb-[240px] mx-[10vw] xl:mx-[240px]">
+            <div className="mx-auto w-full xl:w-[70%]">
+              <video
+                className="hidden-element grid-images w-full object-cover"
+                autoPlay
+                loop
+                muted
+                playsInline
+              >
+                <source src={WeatherCerealVideo01} type="video/mp4" />
+              </video>
+            </div>
+          </section>
 
+          {!!isBrowser && (
+            <section className="mt-[87px] w-[100vw]">
+              <img
+                src={WeatherCerealImage01}
+                className="sticky top-0 w-full h-auto object-cover"
+              />
+              <img
+                src={WeatherCerealImage02}
+                className="sticky top-0 w-full h-auto object-cover"
+              />
+              <img
+                src={WeatherCerealImage03}
+                className="sticky top-0 w-full h-auto object-cover"
+              />
+              <img
+                src={WeatherCerealImage04}
+                className="sticky top-0 w-full h-auto object-cover"
+              />
+            </section>
+          )}
+
+          {!!isMobile && (
+            <>
+              <section className="mt-[87px] mx-[10vw] flex flex-col gap-12">
+                <img
+                  src={WeatherCerealImage01}
+                  className="hidden-element w-full h-auto object-cover"
+                />
+                <img
+                  src={WeatherCerealImage02}
+                  className="hidden-element w-full h-auto object-cover"
+                />
+                <img
+                  src={WeatherCerealImage03}
+                  className="hidden-element w-full h-auto object-cover"
+                />
+                <img
+                  src={WeatherCerealImage04}
+                  className="hidden-element w-full h-auto object-cover"
+                />
+              </section>
+            </>
+          )}
 
           {/* -------------- TO NEXT PROJECT FOOTER PART ------------ */}
 
