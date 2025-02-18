cdcdefbd08ba5c426efe73c928041777b57af105
GentleHorse <anch.abc.t@gmail.com>
2025-02-18 02:19:11 +0100

    Implemeted painting images, 3d art video, 3d web app videos, hologram materials, virtual void space material, ground & fence material

--- src/shaders/holographic/fragment.glsl
+++ src/shaders/holographic/fragment.glsl
@@ -23,21 +23,23 @@ void main() {
     float stripes = mod((vWorldPosition.y - uTime * uStripesSpeed) * uStripesFrequency, 1.0);
     stripes = pow(stripes, uStripesStrength);
 
-    // Fresnel
-    vec3 viewDirection = normalize(vWorldPosition - cameraPosition);
-    float fresnel = dot(viewDirection, normal) + 1.0;
-    fresnel = pow(fresnel, uFresnelStrength);
+    // // Fresnel
+    // vec3 viewDirection = normalize(vWorldPosition - cameraPosition);
+    // float fresnel = dot(viewDirection, normal) + 1.0;
+    // fresnel = pow(fresnel, uFresnelStrength);
 
-    // Falloff
-    float falloff = smoothstep(uFalloffStrength, 0.0, fresnel);
+    // // Falloff
+    // float falloff = smoothstep(uFalloffStrength, 0.0, fresnel);
 
-    // Holographic
-    float holographic = stripes * fresnel;
-    holographic += fresnel * 1.25;  // apply another frensel to strengthen its effect
-    holographic *= falloff; // blur the edges
+    // // Holographic
+    // float holographic = stripes * fresnel;
+    // holographic += fresnel * 1.25;  // apply another frensel to strengthen its effect
+    // holographic *= falloff; // blur the edges
 
     // Final color
-    gl_FragColor = vec4(uColor, holographic);
+    // gl_FragColor = vec4(uColor, holographic);
+    
+    gl_FragColor = vec4(uColor, stripes);
 
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
